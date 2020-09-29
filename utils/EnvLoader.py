573b803efcc979f3190f1132517259782a8b4e80
Martin Ludwig <ML.Commits@gmail.com>
2020-09-29 16:20:40 +0200

    added exception handling to EnvLoader added comments

--- utils/EnvLoader.py
+++ utils/EnvLoader.py
@@ -6,12 +6,16 @@ from config import ROOT_DIR
 
 
 class EnvLoader:
-    def __init__(self, file_path):
-        env_path = path.abspath(path.join(ROOT_DIR, file_path))
-        load_dotenv(dotenv_path=env_path)
 
-    """
-    Reads and returns a value
-    """
+    def __init__(self, file_path: str):
+        """ Loads env file. Filepath starting from ROOT_DIR. """
+        env_path = path.abspath(path.join(ROOT_DIR, file_path))
+
+        if path.exists(env_path):
+            load_dotenv(dotenv_path=env_path)
+        else:
+            raise FileNotFoundError(f"File \"{file_path}\" not found. Could not load environment file.")
+
     def get(self, constant):
+        """ Reads and returns a value """
         return getenv(constant)
