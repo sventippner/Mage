0d00b481ee34e6f31f07d61cdd683eebdbdd3f26
Hell_Shine <~~~~~~~~~~~~>
2020-10-21 14:30:31 +0200

    Purge

--- discord_mage/listeners/OnMessage.py
+++ discord_mage/listeners/OnMessage.py
@@ -9,8 +9,8 @@ class OnMessage:
         if message.author == client.user:
             return
 
-        author = User.find(discord_user_id=message.author.id, discord_guild_id=message.guild.id)
-        self.action_increase_user_points(author)
+#        author = User.find(discord_user_id=message.author.id, discord_guild_id=message.guild.id)
+#        self.action_increase_user_points(author)
 
     def action_increase_user_points(self, user):
         user.points += self.points_per_message
