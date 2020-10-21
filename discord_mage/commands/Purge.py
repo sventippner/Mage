925585fcde49b9a773a0bc53f833c735284d1865
Hell_Shine <~~~~~~~~~~~~>
2020-10-21 14:30:31 +0200

    rebased dev into master

---
+++ discord_mage/commands/Purge.py
@@ -0,0 +1,11 @@
+class Purge:
+    aliases = []
+    brief = 'deletes messages'
+    description = 'deletes the last [amount] messages in the channel. Default value for amount is 2'
+
+    async def call(self, context, amount=2):
+        """ this function is executed by a discord message """
+        if 0 < amount <= 50:
+            await context.channel.purge(limit=amount)
+        else:
+            await context.send(f'{amount} is not a viable amount. You can only purge 1 to 50 messages')
