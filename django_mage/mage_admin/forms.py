d1b2c987c81cc7ce58af32b474ca55d5dcf0a058
Martin Ludwig <ML.Commits@gmail.com>
2020-10-26 18:45:48 +0100

    django work in progress

--- django_mage/mage_admin/forms.py
+++ django_mage/mage_admin/forms.py
@@ -1,11 +1,24 @@
-"""
-from django.forms import *
+
+from django import forms
 
 from discord_mage.models.SimpleDiscordCommand import SimpleDiscordCommand
 
 
-class SimpleDiscordCommandForm(ModelForm):
-    class Meta:
-        model = SimpleDiscordCommand
-        exclude = ()
-"""
+class SimpleDiscordCommandForm(forms.Form):
+    name = forms.CharField(max_length=30)
+    aliases = forms.CharField(max_length=30)
+    brief = forms.CharField(max_length=100)
+    description = forms.CharField(max_length=200)
+
+    # class Meta:
+    #     model = SimpleDiscordCommand
+    #     exclude = ()
+
+    def to_document(self):
+        # Converts this form to a SimpleDiscordCommand document
+        sdc = SimpleDiscordCommand()
+        sdc.name = self.name.to_python()
+        sdc.aliases = self.aliases.to_python().split(",")
+        sdc.brief = self.brief.to_python()
+        sdc.description = self.description.to_python()
+        return sdc
