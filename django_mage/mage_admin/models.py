f3d0b5816bc3f20d52824de6e0a9ed0fb143010d
Martin Ludwig <ML.Commits@gmail.com>
2020-10-28 11:28:41 +0100

    old django web project is now working on mongodb

--- django_mage/mage_admin/models.py
+++ django_mage/mage_admin/models.py
@@ -6,9 +6,9 @@ class SimpleDiscordCommand(models.Model):
         verbose_name_plural = "Simple Disord Commands"
 
     name = models.CharField(max_length=30)
-    output = models.CharField(max_length=500)
     brief = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
+    output = models.CharField(max_length=500)
 
     def __str__(self):
         return f'{self.name}'
