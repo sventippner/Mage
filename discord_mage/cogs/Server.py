34a8fc5211e7a0695d9e19103d8811023466ed2c
chantalfp <fahrenkc@th-brandenburg.de>
2020-10-30 11:35:49 +0100

    added Ping.py and Server.py

---
+++ discord_mage/cogs/Server.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+from discord_mage.commands.Ping import Ping
+
+
+class Server(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(brief=Ping.brief, description=Ping.description)
+    async def ping(self, context):
+        await Ping.call(self, context)
+
+def setup(client):
+    client.add_cog(Server(client))
