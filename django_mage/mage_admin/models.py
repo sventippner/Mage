45fb296d26acd9eec0b4cb6c959f0e8720ee71ab
Martin Ludwig <ML.Commits@gmail.com>
2020-11-13 14:55:58 +0100

    django forms

--- django_mage/mage_admin/models.py
+++ django_mage/mage_admin/models.py
@@ -3,7 +3,7 @@ from djongo import models
 
 class SimpleDiscordCommand(models.Model):
     class Meta:
-        verbose_name_plural = "Simple Disord Commands"
+        verbose_name_plural = "Simple Discord Commands"
 
     name = models.CharField(max_length=30)
     brief = models.CharField(max_length=100)
@@ -12,3 +12,28 @@ class SimpleDiscordCommand(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+
+class BotSettingsModel(models.Model):
+    class Meta:
+        verbose_name_plural = "Bot Settings"
+
+    bot_owner = models.TextField()
+
+
+class ServerSettingsModel(models.Model):
+    class Meta:
+        verbose_name_plural = "Server Settings"
+
+    discord_server_id = models.CharField(max_length=200)
+    prefix = models.CharField(max_length=30)
+    points_name = models.CharField(max_length=30)
+    bot_owner = models.TextField()
+
+
+class SdcSettingsModel(models.Model):
+    class Meta:
+        verbose_name_plural = "SDC Settings"
+
+    sdc_path = models.CharField(max_length=30)
+    sdc_name = models.CharField(max_length=20)
