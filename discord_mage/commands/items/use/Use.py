df0077509330394e36814e1831b7d49417a02f5e
Sven Tippner <sven.tippner@googlemail.com>
2020-12-14 16:41:40 +0100

    deleted unnecessary lines of code in Use.py

--- discord_mage/commands/items/use/Use.py
+++ discord_mage/commands/items/use/Use.py
@@ -6,11 +6,3 @@ class Use:
     @staticmethod
     async def call(context):
         return
-
-    @staticmethod
-    def action_invalid_argument(argument):
-        return f"{argument} is an invalid argument"
-
-    @staticmethod
-    def action_set_argument(argument, value):
-        return f"{argument} are now {value}"
